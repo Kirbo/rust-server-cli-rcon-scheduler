85e39a3bb0a8f8a39ce90ddbdfc1e07ec6cbe931
Kimmo Saari <kimmo.saari@revolt.fi>
2017-04-12 17:39:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# rust-server-cli-rcon-scheduler
+Rust Server CLI RCON Scheduler
+
+# Installation
+
+#### Installing Node.js
+Check the instructions how to install Node.js and NPM on your OS:
+https://nodejs.org/en/download/package-manager/
+
+#### Installing the rust-server-cli-rcon
+
+    # Clone this repository
+    git clone https://github.com/kirbo/rust-server-cli-rcon-scheduler.git
+    
+    # Change directory
+    cd rust-server-cli-rcon-scheduler
+ 
+    # Install dependencies
+    npm install
+ 
+    # Edit both "rcon.json" and "schedules.json" files, with your favorite editor, e.g.
+    nano rcon.json schedules.json
+    
+    # Make sure you have "+rcon.web true" in your Rust server,
+    # otherwice this wont work (at least for now)..
+
+    # Start the scheduler
+    node index.js
+
+# Usage
+
+You can set commands in the `schedules.json` file via 2 different ways, such as:
+
+    [
+        {
+            "command": "say This message will be send every minute",
+            "interval": 1
+        }
+    ]
+    
+Or if you want the command to be executed after specified time:
+
+    [
+        {
+            "command": "say This message will be sent only once, after the given time below",
+            "after": "2017-04-12 17:15:00"
+        }
+    ]
+
+The message above will be triggered only once (after the `schedules.json` has been loaded).
+
+Messages need to have `command` and either of these: `interval` or `after`.
+
+You can modify the `schedules.json` while the script is running. It will check for changes in the file every 10 seconds, so you don't need to restart the script if you make changes.
+
+#### Exiting the client
+
+Simply press **CTRL + C** and the scheduler will exit.
+
+# Motivation
+A man named Simon contacted me via e-mail, because of my other project [rust-server-cli-rcon](https://github.com/kirbo/rust-server-cli-rcon) and asked can he execute timed commands with it, so I offered that I can create such tool for him and here it is.
---
+++ index.js
@@ -0,0 +1,146 @@
+var W3CWebSocket = require('websocket').w3cwebsocket;
+var Promise = require('promise');
+var rconConfig = require('./rcon.json');
+var fs = require('fs');
+
+var checkNewSchedules = 10; // Seconds
+
+
+/* Don't touch anything below unless you know what you're doing */
+var rcon;
+var schedules = [];
+var timers = [];
+var toBeExecuted = new Set();
+var first = true;
+var second = 1000;
+var minute = 60 * second;
+var offset = 5;
+
+function text(text, border) {
+    console.log('[' + new Date + '] ' + text);
+    if (border) {
+        console.log('****************' + "\n");
+    }
+}
+
+function rconConnect() {
+    return new Promise(function (resolve, reject) {
+        var count = 1;
+        text('RCON: Connecting...');
+        rcon = new W3CWebSocket('ws://' + rconConfig.host + ':' + rconConfig.port + '/' + rconConfig.pass);
+        rcon.onopen = function () {
+            text('RCON: Connected');
+            resolve(rcon);
+        };
+        rcon.onerror = function (error) {
+            if (count > 10) {
+                text('RCON: Connection failed!');
+                reject(rcon);
+            }
+            text('RCON: Retrying connection in 5 seconds');
+            setTimeout(function () {
+                return rconConnect()
+                    .then(
+                        function () {
+                            resolve();
+                        },
+                        function (error) {
+                            reject(error);
+                        }
+                    );
+            }, 5 * 1000);
+            count++;
+        };
+    });
+}
+
+function rconCmd(command) {
+    text('RCON: Sending command: ' + command);
+    return new Promise(function (resolve, reject) {
+        rcon.send(JSON.stringify({
+            Identifier: -1,
+            Message: command,
+            Name: "WebRcon"
+        }));
+        resolve(rcon);
+    });
+}
+
+function readSchedules(file) {
+    return JSON.parse(fs.readFileSync(file));
+}
+
+function loadSchedules() {
+    var newSchedules = readSchedules('./schedules.json');
+    if (JSON.stringify(schedules) !== JSON.stringify(newSchedules)) {
+        if (!first) {
+            text('New schedules loaded!');
+            clearTimers();
+            text('Old timers cleared');
+        }
+        schedules = newSchedules;
+        createTimers();
+        text('New schedules initialised, waiting for '+offset+' before setting timers.');
+        first = false;
+    }
+}
+
+function createTimer(schedule) {
+    setTimeout(function() {
+        var timer = setInterval(function() {
+            executeCommand(schedule);
+        }, schedule.interval * minute);
+        timers.push(timer);
+    }, offset * 1000);
+}
+
+function executeCommand(schedule) {
+    return new Promise(function (resolve, reject) {
+        rconCmd(schedule.command).then(function () {
+            resolve(true);
+        });
+    });
+}
+
+function checkAfter() {
+    toBeExecuted.forEach(function(schedule) {
+        if (schedule.after && new Date(schedule.after) < new Date()) {
+            executeCommand(schedule);
+            toBeExecuted.delete(schedule);
+        }
+    });
+}
+
+function createTimers() {
+    schedules.map(function(schedule) {
+        if (schedule.interval) {
+            createTimer(schedule);
+        }
+        else if (schedule.after) {
+            toBeExecuted.add(schedule);
+        }
+    });
+}
+
+function clearTimers() {
+    timers.map(function(timer) {
+        clearInterval(timer);
+    });
+}
+
+function startMainInterval() {
+    loadSchedules();
+    setInterval(function() {
+        loadSchedules();
+        checkAfter();
+    }, checkNewSchedules * second);
+}
+
+rconConnect()
+    .then(function () {
+        startMainInterval();
+    })
+    .catch(function (error) {
+        console.error('Error:', error);
+    })
+;
